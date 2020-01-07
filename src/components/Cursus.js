c5628fb01f42a54a1a0e567343f088ab9d5bfcfe
Jawher Hamza <jawher.hamza@hsoumi.com>
2020-01-07 23:43:00 +0100

    HELLO

--- src/components/Cursus.js
+++ src/components/Cursus.js
@@ -1,15 +1,15 @@
-import React, { } from 'react';
-import { FormattedDate, FormattedMessage } from 'react-intl';
-import '../styles/Intro.css'
+import React from "react";
+import "../styles/Cursus.css";
 
-function Intro() {
-    return (
-        <div className="cursus">
-            <div className="dark-overlay">
-                CURSUS
-            </div>
-        </div>
-    );
+export default function Cursus() {
+  return (
+    <div className="cursus">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam labore
+      cumque tempora est modi? Laboriosam asperiores harum distinctio ducimus
+      hic dicta debitis blanditiis quisquam. Perferendis repudiandae omnis,
+      atque error ipsam adipisci, sapiente, impedit voluptatem aperiam incidunt
+      facilis pariatur cum necessitatibus corrupti fugit animi? Tempora iusto
+      saepe ab quaerat explicabo! Sapiente!
+    </div>
+  );
 }
-
-export default Intro;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import Zoom from '@material-ui/core/Zoom';
-import { Slide, IconButton, Menu, MenuItem } from '@material-ui/core';
-import LanguageIcon from '@material-ui/icons/Language';
-import Intro from './Intro'
-import '../styles/NavBar.css'
-
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Container from "@material-ui/core/Container";
+import Fab from "@material-ui/core/Fab";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Zoom from "@material-ui/core/Zoom";
+import { Slide, IconButton, Menu, MenuItem } from "@material-ui/core";
+import LanguageIcon from "@material-ui/icons/Language";
+import Intro from "./Intro";
+import Cursus from "./Cursus";
+import "../styles/NavBar.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    position: 'fixed',
+    position: "fixed",
     bottom: theme.spacing(2),
-    right: theme.spacing(2),
+    right: theme.spacing(2)
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 function ScrollTop(props) {
@@ -37,13 +37,15 @@ function ScrollTop(props) {
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
     disableHysteresis: true,
-    threshold: 100,
+    threshold: 100
   });
 
   const handleClick = event => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
     if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   };
 
@@ -75,19 +77,22 @@ export default function NavBar({ setLanguage }) {
     <React.Fragment>
       <CssBaseline />
       <HideOnScroll>
-        <AppBar >
-          <Toolbar >
+        <AppBar>
+          <Toolbar>
             <Typography variant="h6" className={classes.title}>
               <span className="navbar-brand">
-                <i id="dash" class="fa fa-code" />  Jawher Hamza  <i id="dash2" class="fa fa-code" />
+                <i id="dash" class="fa fa-code" /> Jawher Hamza
+                <i id="dash2" class="fa fa-code" />
               </span>
             </Typography>
             <IconButton
-              style={{ float: 'right' }}
+              style={{ float: "right" }}
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={(event) => { setAnchorEl(event.currentTarget) }}
+              onClick={event => {
+                setAnchorEl(event.currentTarget);
+              }}
               color="inherit"
             >
               <LanguageIcon />
@@ -96,37 +101,58 @@ export default function NavBar({ setLanguage }) {
               id="menu-appbar"
               anchorEl={anchorEl}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right"
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right"
               }}
               open={open}
-              onClose={() => { setAnchorEl(null) }}
+              onClose={() => {
+                setAnchorEl(null);
+              }}
             >
-              <MenuItem onClick={() => { setLanguage('en') }}>EN <img src='https://cometrip.sn/_nuxt/img/e53dbca.png' className="language-ico"></img></MenuItem>
-              <MenuItem onClick={() => { setLanguage('fr') }}>FR <img src='https://cometrip.sn/_nuxt/img/d892704.png' className="language-ico"></img></MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setLanguage("en");
+                }}
+              >
+                English{" "}
+                <img
+                  src="https://cometrip.sn/_nuxt/img/e53dbca.png"
+                  className="language-ico"
+                ></img>
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  setLanguage("fr");
+                }}
+              >
+                Français{" "}
+                <img
+                  src="https://cometrip.sn/_nuxt/img/d892704.png"
+                  className="language-ico"
+                ></img>
+              </MenuItem>
             </Menu>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
       <Toolbar id="back-to-top-anchor" />
-      <Container>
-        <Intro></Intro>
-      </Container>
+      <Intro />
+      <Cursus />
       <ScrollTop>
         <Fab color="secondary" size="small" aria-label="Scroll Back To Top">
           <KeyboardArrowUpIcon />
         </Fab>
       </ScrollTop>
-    </React.Fragment >
+    </React.Fragment>
   );
 }
 
 ScrollTop.propTypes = {
   children: PropTypes.element.isRequired,
-  window: PropTypes.func,
+  window: PropTypes.func
 };
